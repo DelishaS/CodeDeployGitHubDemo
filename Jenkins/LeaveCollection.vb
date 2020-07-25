d738a7cdb0dc5a8cc550cec27777fe385166f084
Jyoti <kjyoti@dxc.com>
2020-07-24 22:53:35 -0400

    Build1

---
+++ Jenkins/LeaveCollection.vb
@@ -0,0 +1,33 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Collections
+Imports System.Collections.Generic
+Public Class LeaveCollection
+    Private _Empnoo As Int32
+
+    Private _LeaveRecord As List(Of String)
+
+    Public Property empno() As Int32
+        Get
+            Return empno
+
+        End Get
+        Set(ByVal value As Int32)
+            _Empnoo = value
+
+        End Set
+    End Property
+
+ 
+    Public Property LeaveRecord() As List(Of String)
+        Get
+            Return _LeaveRecord
+        End Get
+        Set(ByVal value As List(Of String))
+            _LeaveRecord = value
+
+        End Set
+    End Property
+   
+
+
+End Class
